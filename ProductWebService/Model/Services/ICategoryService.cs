723e0c7cd0b28f7cf65c52aa3cf10c35bd750a23
htavakkoli1368 <32728223+htavakkoli1368@users.noreply.github.com>
2025-04-11 01:38:00 +0800

    add product and category service

--- ProductWebService/Model/Services/ICategoryService.cs
+++ ProductWebService/Model/Services/ICategoryService.cs
@@ -1,6 +1,50 @@
-﻿namespace ProductWebService.Model.Services
+﻿using ProductWebService.Infrastructure.Context;
+using ProductWebService.Model.Entity;
+
+namespace ProductWebService.Model.Services
 {
     public interface ICategoryService
     {
+        List<CategoryDto> GetCategory();
+        void AddCategory(CategoryDto category);
+    }
+
+    class CategoryService : ICategoryService
+    {
+        private readonly ProductDatabaseContext databaseContext;
+
+        public CategoryService(ProductDatabaseContext databaseContext)
+        {
+            this.databaseContext = databaseContext;
+        }
+        public void AddCategory(CategoryDto category)
+        {
+            Category categ = new Category
+            {
+                 Name= category.Name,
+                 Description= category.Description,
+            };
+            databaseContext.Categories.Add(categ);
+            databaseContext.SaveChanges();
+        }
+
+        public List<CategoryDto> GetCategory()
+        {
+            var category = databaseContext.Categories
+                          .OrderBy(c => c.Name)
+                          .Select(p => new CategoryDto
+                          {
+                              Name=p.Name,
+                              Description=p.Description
+                          }).ToList();
+            return category;
+        }
+    }
+
+    public class CategoryDto
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
     }
 }
--- ProductWebService/Model/Services/IProductService.cs
+++ ProductWebService/Model/Services/IProductService.cs
@@ -1,6 +1,86 @@
-﻿namespace ProductWebService.Model.Services
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
+using ProductWebService.Infrastructure.Context;
+using ProductWebService.Model.Entity;
+
+namespace ProductWebService.Model.Services
 {
     public interface IProductService
     {
+        List<ProductDto> GetProductsList();
+        ProductDto GetProduct(Guid id);
+    }
+
+    public class ProductService : IProductService
+    {
+        private readonly ProductDatabaseContext databaseContext;
+
+        public ProductService(ProductDatabaseContext databaseContext)
+        {
+            this.databaseContext = databaseContext;
+        }
+
+        public ProductDto GetProduct(Guid id)
+        {
+            var product = databaseContext.Products                        
+                          .SingleOrDefault(c => c.Id==id);
+
+            if (product == null)
+                throw new Exception("product not found");
+            var data = new ProductDto
+            {
+                Id = product.Id,
+                Description = product.Description,
+                Image = product.Image,
+                Name = product.Name,
+                Price = product.Price,
+                ProductCategory = new ProductCategoryDto
+                {
+                    Category = product.Category.Name,
+                    CategoryId = product.Category.Id
+                }
+            };
+            return data;
+        }
+
+        public List<ProductDto> GetProductsList()
+        {
+            var products = databaseContext.Products
+                           .Include(p=>p.Category)
+                           .OrderByDescending(p => p.Id)
+                           .Select(p => new ProductDto
+                           {
+                               Id = p.Id,
+                               Description = p.Description,
+                               Image= p.Image,
+                               Name = p.Name,
+                               Price = p.Price,
+                               ProductCategory = new ProductCategoryDto
+                               {
+                                   Category = p.Category.Name,
+                                   CategoryId=p.Category.Id
+                               }
+                           }).ToList();
+            return products;
+        }
+    }
+
+    public class ProductDto
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? Image { get; set; }
+        public int Price { get; set; }
+
+        public ProductCategoryDto ProductCategory { get; set; }
+
+    }
+
+    public class ProductCategoryDto
+    {
+        public Guid CategoryId { get; set; }
+
+        public string Category { get; set; }
     }
 }
